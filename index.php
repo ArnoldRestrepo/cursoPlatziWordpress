910f9ea78dc120db0ea8c7970d394f756ba0ccf7
Arnold Retrepo <restrepo_arnold@outlook.com>
2017-01-14 16:00:41 -0500

    Loop y Filtros para Wordpress

--- index.php
+++ index.php
@@ -7,17 +7,25 @@
                     Últimos Trabajos
                 </div>
                 <div class="block__body grid">
-                  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                    <article class="block grid--item-6">
-                      <h2 class="block__title"><?php the_title(); ?></h2>
-                      <div class="block__body">
-                        <p><?php the_excerpt(); ?></p>
-                        <footer>
-                          <small><?php the_tags(); ?> <?php the_author(); ?></small>
-                        </footer>
-                      </div>
-                    </article>
-                  <?php endwhile; else : ?>
+                  <?php
+                    $args = array('author' => "admin");
+                    $filter_posts = new WP_Query( $args );
+                    if ($filter_posts->have_posts() ) : while ( $filter_posts->have_posts() ) : $filter_posts->the_post();
+                  ?>
+                        <article class="block grid--item-6">
+                          <h2 class="block__title"><?php the_title(); ?></h2>
+                          <div class="block__body">
+                            <p><?php the_excerpt(); ?></p>
+                            <footer>
+                              <small><?php the_tags(); ?> <?php the_author(); ?></small>
+                              <a href="<?php the_permalink(); ?>">Leer más...</a>
+                            </footer>
+                          </div>
+                        </article>
+                  <?php
+                      endwhile;
+                      else :
+                  ?>
                     <p><?php _e( 'Lo sentimos no hay contenido' ); ?></p>
                   <?php endif; ?>
                 </div>
